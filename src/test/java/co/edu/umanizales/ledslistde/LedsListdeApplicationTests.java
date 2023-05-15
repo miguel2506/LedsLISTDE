ed53de6366c76b92cd904a20dbc7c424b3e12013
miguel <miguel.giraldom2506@gmail.com>
2023-05-15 15:39:40 -0500

    añadi unas cosas

---
+++ src/test/java/co/edu/umanizales/ledslistde/LedsListdeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.ledslistde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedsListdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
